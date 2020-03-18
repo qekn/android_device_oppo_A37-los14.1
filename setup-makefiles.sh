3ea7ce7bc4283e42e17f6d813565bf47026adc8d
Jakesoso <1343938313@qq.com>
2020-03-18 23:08:11 +0800

    A37: Correct blobs and related script
     * Correct some stupid mistakes

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -121,7 +121,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MULTILIB := 32
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libdpframework.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libdpframework.so
 include \$(BUILD_PREBUILT)
 
 include \$(CLEAR_VARS)
@@ -131,7 +131,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MULTILIB := 64
-LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libdpframework.so
+LOCAL_SRC_FILES_64 := proprietary/lib64/libdpframework.so
 include \$(BUILD_PREBUILT)
 
 endif

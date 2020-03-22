39aeb6f9f4cf8b60a154b095eff0a7390f99e6cc
Jakesoso <1343938313@qq.com>
2020-03-22 12:20:08 +0800

    A37: Update rootdir

--- device_A37.mk
+++ device_A37.mk
@@ -209,11 +209,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Ramdisk files
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/factory_init.project.rc:root/factory_init.project.rc \
+    $(LOCAL_PATH)/rootdir/factory_init.rc:root/factory_init.rc \
+    $(LOCAL_PATH)/rootdir/init.common_svc.rc:root/init.common_svc.rc \
+    $(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
+    $(LOCAL_PATH)/rootdir/init.project.rc:root/init.project.rc \
+    $(LOCAL_PATH)/rootdir/meta_init.modem.rc:root/meta_init.modem.rc \
+    $(LOCAL_PATH)/rootdir/meta_init.project.rc:root/meta_init.project.rc \
+    $(LOCAL_PATH)/rootdir/meta_init.rc:root/meta_init.rc \
     $(LOCAL_PATH)/rootdir/fstab.mt6755:root/fstab.mt6755 \
     $(LOCAL_PATH)/rootdir/init.mt6755.rc:root/init.mt6755.rc \
     $(LOCAL_PATH)/rootdir/init.mt6755.usb.rc:root/init.mt6755.usb.rc \
-    $(LOCAL_PATH)/rootdir/ueventd.mt6755.rc:root/ueventd.mt6755.rc \
-	$(LOCAL_PATH)/rootdir/init.mt6755.modem.rc:root/init.mt6755.modem.rc
+    $(LOCAL_PATH)/rootdir/ueventd.mt6755.rc:root/ueventd.mt6755.rc
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \

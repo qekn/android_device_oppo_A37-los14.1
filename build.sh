7ccc5605431b5b237e64339651c6da90b94fb66f
jakesoso <Jakesoso>
2020-04-18 21:15:13 +0800

    Update for github actions

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ repo init --depth=1 -u git://github.com/LineageOS/android.git -b cm-14.1 && repo
 mkdir -p device/oppo
 git clone https://github.com/Jakesoso/android_device_oppo_A37-los14.1.git device/oppo/A37
 sh device/oppo/A37/patches/install.sh
-#source build/envsetup.sh && brunch lineage_A37-eng
+source build/envsetup.sh && brunch lineage_A37-eng
